ad9b7d4dcedfc1c152ab7286c48f6c40d579b44e
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-07 15:34:22 -0800

    my dogedit and doglist also passed

--- src/Views/Dogs/DogEdit.test.js
+++ src/Views/Dogs/DogEdit.test.js
@@ -4,10 +4,10 @@ import DogEdit from './DogEdit';
 
 test('renders dog information', async () => {
   const { container } = render(
-    <MemoryRouter initialEntries={['/dogs/1/edit']}>
+    <MemoryRouter initialEntries={['/dogs/2/edit']}>
       <Route path="/dogs/:id/edit" component={DogEdit} />
     </MemoryRouter>
   );
-  await screen.findByDisplayValue('Miklo');
+  await screen.findByDisplayValue('Luna');
   expect(container).toMatchSnapshot();
 });
