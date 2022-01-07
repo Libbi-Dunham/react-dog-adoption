14122d14f1d89fe9b526613ea1124cc93f3542d1
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-06 17:46:47 -0800

    moved my delete button

--- src/Components/DogCard/Dogs.js
+++ src/Components/DogCard/Dogs.js
@@ -3,12 +3,16 @@ import React from 'react';
 
 export default function Dogs({ id, name, image, bio, handleDelete }) {
   return (
-    <div className="dog">
-      <h2> {name} </h2>
-      <img src={image}></img>
-      <h2> {bio} </h2>
-      <Link to={`/dogs/${id}/DogEdit`}>Edit</Link>
-      <button onClick={handleDelete}>delete</button>
-    </div>
+    <>
+      <div className="dog">
+        <h2> {name} </h2>
+        <img src={image}></img>
+        <h2> {bio} </h2>
+        <Link to={`/dogs/${id}/DogEdit`}>Edit</Link>
+      </div>
+      <div>
+        <button onClick={handleDelete}>delete</button>
+      </div>
+    </>
   );
 }
